71b0951ecaa4feb5253c10beae36db3cf7541514
alstn2468 <alstn2468_@naver.com>
2020-12-09 22:47:43 +0900

    Clean all schema, resolver and setup env configuration

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,6 @@
-import { getMovies, getById, addMovie, deleteMovie } from '../database';
-
 const resolvers = {
-  Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
-  }
+  Query: { },
+  Mutation: {},
 };
 
 export default resolvers;
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import { GraphQLServer } from 'graphql-yoga';
+import dotenv from 'dotenv';
 import resolvers from './graphql/resolvers'
+
+dotenv.config({ path: `/env/${process.env.NODE_ENV}` });
+
+const options = {
+  port: process.env.PORT || 4000,
+  endpoint: process.env.ENDPOINT || '/graphql',
+  playground: process.env.PLAYGROUND || false,
+};
 
 const server = new GraphQLServer({
   typeDefs: 'graphql/schema.graphql',
   resolvers,
 });
 
-server.start(() => console.log('GraphQL Server Running on localhost:4000'));
+server.start(
+  options, 
+  () => {
+    console.log('GraphQL Server Running on localhost:4000');
+    console.log(`Running options :${JSON.stringify(options)}`);
+  },
+);
